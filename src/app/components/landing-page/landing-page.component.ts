5312f65d69e87aaf25fbae2d72055ba5ff4963d7
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-09-05 18:02:23 +0300

    Added login and register

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-landing-page',
@@ -11,25 +10,8 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(function() {
 
-      $('#login-form-link').click(function(e) {
-      $("#login-form").delay(100).fadeIn(100);
-       $("#register-form").fadeOut(100);
-      $('#register-form-link').removeClass('active');
-      $(this).addClass('active');
-      e.preventDefault();
-    });
-    $('#register-form-link').click(function(e) {
-      $("#register-form").delay(100).fadeIn(100);
-       $("#login-form").fadeOut(100);
-      $('#login-form-link').removeClass('active');
-      $(this).addClass('active');
-      e.preventDefault();
-    });
-  
-  });
-  
+
   }
 
 }
