352a1fa153cc683dfdbd22190d52abff99c08592
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-09-05 19:17:40 +0300

    Login/register fixed

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
 
 const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     { path: '', component: LandingPageComponent},
     { path: '**', component: ErrorComponent}
 ]
