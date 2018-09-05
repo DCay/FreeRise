2e67c81c9529ecf69fd8eb66ac51a25666063be2
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-09-05 16:00:44 +0300

    Add 3 components, landing-page,navigation,footer
    We also impoted bootstrap and jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavigationComponent } from './components/shared-components/navigation/navigation.component';
+import { FooterComponent } from './components/shared-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
